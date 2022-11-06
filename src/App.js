f36a24492428231a1c0beb98fafb6886b12761f2
ocastell24 <ocastell@hotmail.com>
2022-11-05 21:29:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react-v1'
 
+import { Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Auth!</h1>
+        <Route path="index" render={() => {window.location.href="index.html"}} />
+
       </header>
       <AmplifySignOut />
     </div>
